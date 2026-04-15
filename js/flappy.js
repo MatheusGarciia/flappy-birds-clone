fcd66c8d925eae02426cd0da59d3964aedc23025
Matheus Garcia <garciiamatheus98@gmail.com>
2026-04-15 17:29:38 -0300

    obstacles configs

--- js/flappy.js
+++ js/flappy.js
@@ -26,15 +26,25 @@ function ObstaclePair(height, opening, x) {
   this.top = new Obstacle(true);
   this.bottom = new Obstacle(false);
 
+  this.top.element.classList.add("top");
+  this.bottom.element.classList.add("bottom");
+
   this.element.appendChild(this.top.element);
   this.element.appendChild(this.bottom.element);
 
   this.sortOpening = () => {
-    const heightTop = Math.random() * (height - opening);
-    const heightBottom = height - opening - heightTop;
+    const openingHeight = opening;
+    const minBottom = height * 0.15;
+
+    const maxTop = height - openingHeight - minBottom;
+
+    const heightTop = Math.random() * maxTop;
+    const heightBottom = height - openingHeight - heightTop;
 
     this.top.setHeight(heightTop);
     this.bottom.setHeight(heightBottom);
+
+    this.bottom.element.style.top = `${heightTop + openingHeight}px`;
   };
 
   this.setX = (x) => {
@@ -50,6 +60,43 @@ function ObstaclePair(height, opening, x) {
   this.sortOpening();
   this.setX(x);
 }
-
+/*
 const b = new ObstaclePair(700, 200, 800);
-document.querySelector("[tp-flappy]").appendChild(b.element);
+document.querySelector("[tp-flappy]").appendChild(b.element);*/
+
+function Obstacles(height, width, opening, space, notifyPoint) {
+  this.pairs = [
+    new ObstaclePair(height, opening, width),
+    new ObstaclePair(height, opening, width + space),
+    new ObstaclePair(height, opening, width + space * 2),
+    new ObstaclePair(height, opening, width + space * 3),
+  ];
+
+  const displacement = 3;
+
+  this.animate = () => {
+    this.pairs.forEach((pair) => {
+      pair.setX(pair.getX() - displacement);
+
+      if (pair.getX() < -pair.getWidth()) {
+        const maxX = Math.max(...this.pairs.map((p) => p.getX()));
+        pair.setX(maxX + space);
+        pair.sortOpening();
+      }
+
+      const middle = width / 2;
+      const crossTheMiddle =
+        pair.getX() + displacement >= middle && pair.getX() < middle;
+      if (crossTheMiddle) notifyPoint();
+    });
+  };
+}
+
+const obstacles = new Obstacles(700, 1200, 200, 400);
+
+const gameArea = document.querySelector("[tp-flappy]");
+obstacles.pairs.forEach((pair) => gameArea.appendChild(pair.element));
+
+setInterval(() => {
+  obstacles.animate();
+}, 20);
